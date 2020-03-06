2ae72b8b183a18b958440a0c039973f5c91ab73f
Steve <ycsteven.tsao@gmail.com>
2020-03-06 15:58:57 +0800

    使用 getFieldProps 簡化表單屬性

--- src/EmailInput.js
+++ src/EmailInput.js
@@ -52,35 +52,14 @@ const EmailInput = () => {
     return (
         <form onSubmit={formik.handleSubmit}>
             <label htmlFor="firstName">First Name</label>
-            <input
-                id="firstName"
-                name="firstName"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.firstName}
-            />
+            <input name="firstName" {...formik.getFieldProps('firstName')} />
             {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
             <label htmlFor="lastName">Last Name</label>
-            <input
-                id="lastName"
-                name="lastName"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.lastName}
-            />
+            <input name="lastName" {...formik.getFieldProps('lastName')} />
             {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
             <label htmlFor="email">Email Address</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-            />
+            <input name="email" {...formik.getFieldProps('email')} />
             {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
             <div id="submit">
                 <button type="submit">Submit</button>
