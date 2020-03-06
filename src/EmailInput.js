c2087f7f1d14a458735df5acc35dcd1460967b61
Steve <ycsteven.tsao@gmail.com>
2020-03-06 15:49:46 +0800

    用 yup 驗證表單

--- src/EmailInput.js
+++ src/EmailInput.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './style.css';
 
-const validate = values => {
-    const errors = {};
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-        errors.firstName = 'Must be 15 characters or less';
-    }
+// const validate = values => {
+//     const errors = {};
+//     if (!values.firstName) {
+//         errors.firstName = 'Required';
+//     } else if (values.firstName.length > 15) {
+//         errors.firstName = 'Must be 15 characters or less';
+//     }
 
-    if (!values.lastName) {
-        errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less';
-    }
+//     if (!values.lastName) {
+//         errors.lastName = 'Required';
+//     } else if (values.lastName.length > 20) {
+//         errors.lastName = 'Must be 20 characters or less';
+//     }
 
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
+//     if (!values.email) {
+//         errors.email = 'Required';
+//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//         errors.email = 'Invalid email address';
+//     }
 
-    return errors;
-};
+//     return errors;
+// };
 
 const EmailInput = () => {
     const formik = useFormik({
@@ -33,7 +34,17 @@ const EmailInput = () => {
             phone: '',
             email: ''
         },
-        validate,
+        validationSchema: Yup.object({
+            firstName: Yup.string()
+                .max(15, '只能比15個字少')
+                .required('必填'),
+            lastName: Yup.string()
+                .max(20, '只能比20個字少')
+                .required('必填'),
+            email: Yup.string()
+                .email('不符合 email 格式')
+                .required('必填')
+        }),
         onSubmit: values => {
             console.log(JSON.stringify(values, null, 2));
         }
@@ -46,22 +57,31 @@ const EmailInput = () => {
                 name="firstName"
                 type="text"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.firstName}
             />
-            {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+            {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
             <label htmlFor="lastName">Last Name</label>
             <input
                 id="lastName"
                 name="lastName"
                 type="text"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.lastName}
             />
-            {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+            {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
             <label htmlFor="email">Email Address</label>
-            <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
-            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
             <div id="submit">
                 <button type="submit">Submit</button>
             </div>
